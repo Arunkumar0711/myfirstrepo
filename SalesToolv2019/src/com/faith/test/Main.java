d06f65c60b212e9915b1dffababc335479417e33
dev23 <dev23@dev23-desktop>
2019-09-24 14:53:13 +0530

    firstcommit

---
+++ SalesToolv2019/src/com/faith/test/Main.java
@@ -0,0 +1,17 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData obj = new SalesData();
+		System.out.println("Welcome to GitRepository");
+		displayGreeting();
+		obj.display();
+	}
+	private static void displayGreeting(){
+		System.out.println("Static Function display");
+		System.out.println("Displaying static function");
+	}
+
+}
